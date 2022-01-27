1364d5b65f6d903c2f2ff3c70ba1f3ee392547c9
eticialima <contato.leticialima.me@gmail.com>
2022-01-27 16:45:32 -0300

    ok

--- franquise-api/core/views.py
+++ franquise-api/core/views.py
@@ -4,39 +4,10 @@ from rest_framework import viewsets
 
 class FranchiseViewSet(viewsets.ModelViewSet): 
     queryset = Franchise.objects.all() 
-    serializer_class = FranchiseSerializer 
-     
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def update(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request):
-        return self.delete(request)
-
+    serializer_class = FranchiseSerializer  
+    
 class ListFranchiseViewSet(viewsets.ModelViewSet): 
     queryset = ListFranchise.objects.all() 
     serializer_class = ListFranchiseSerializer 
-     
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def update(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request):
-        return self.delete(request)
+    
 
