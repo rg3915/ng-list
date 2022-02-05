88840b60920382fb1d982c16cd4b3b53948a9b90
Regis Santos <regis42santos@gmail.com>
2022-02-04 23:38:52 -0300

    Define read_only=True em FranchiseSerializer.

--- franchise/core/serializers.py
+++ franchise/core/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Franchise, Collectable
+
+from .models import Collectable, Franchise
 
 
 class FranchiseSerializer(serializers.ModelSerializer):
@@ -16,8 +17,16 @@ class FranchiseSerializer(serializers.ModelSerializer):
 class CollectableSerializer(serializers.ModelSerializer):
     '''
     Coleção Completa
+
+    O read_only=True resolve o bug
+
+    AssertionError: The `.update()` method does not support writable
+    nested fields by default.
+    Write an explicit `.update()` method for
+    serializer `core.serializers.CollectableSerializer`,
+    or set `read_only=True` on nested serializer fields.
     '''
-    franchise = FranchiseSerializer(many=True, required=False)
+    franchise = FranchiseSerializer(many=True, read_only=True)
 
     class Meta:
         model = Collectable
