dd47d4fffbf822c4e48ba7ca5e1134beab7e49e0
Regis Santos <regis42santos@gmail.com>
2022-02-05 00:01:46 -0300

    Adiciona create_or_update_franchise

--- franchise/core/views.py
+++ franchise/core/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from .models import Collectable, Franchise
 from .serializers import CollectableSerializer, FranchiseSerializer
@@ -10,6 +9,22 @@ class CollectableFranchiseViewSet(viewsets.ModelViewSet):
     serializer_class = FranchiseSerializer
 
 
+def create_or_update_franchise(franchise, instance):
+    for item in franchise:
+        _id = item.get('id')
+        if _id:
+            # Pega Franchise existente
+            franchise_obj = instance.franchise.get(id=_id)
+            franchise_obj.value = item.get('value')
+            franchise_obj.quantity = item.get('quantity')
+            franchise_obj.save()
+        else:
+            instance.franchise.create(
+                value=item.get('value'),
+                quantity=item.get('quantity'),
+            )
+
+
 class CollectableViewSet(viewsets.ModelViewSet):
     queryset = Collectable.objects.all()
     serializer_class = CollectableSerializer
@@ -18,28 +33,11 @@ class CollectableViewSet(viewsets.ModelViewSet):
         '''
         https://www.cdrf.co/3.12/rest_framework.viewsets/ModelViewSet.html#update
         '''
-        # Reescreve o update default.
-        partial = kwargs.pop('partial', False)
         instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
 
         franchise = self.request.data.get('franchise')
 
         if franchise:
-            for item in franchise:
-                _id = item.get('id')
-                if _id:
-                    # Pega Franchise existente
-                    franchise_obj = instance.franchise.get(id=_id)
-                    franchise_obj.value = item.get('value')
-                    franchise_obj.quantity = item.get('quantity')
-                    franchise_obj.save()
-                else:
-                    instance.franchise.create(
-                        value=item.get('value'),
-                        quantity=item.get('quantity'),
-                    )
+            create_or_update_franchise(franchise, instance)
 
-        return Response(serializer.data)
+        return super(CollectableViewSet, self).update(request, *args, **kwargs)
